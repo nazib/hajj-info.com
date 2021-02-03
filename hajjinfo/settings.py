f660d39237e562a302dc5d140f08817e9ddb8c0b
nazib <abdullah.nazib@gmail.com>
2021-02-03 20:29:39 +1000

    adding database from digitalocean

--- hajjinfo/settings.py
+++ hajjinfo/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'hajjinfo.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+'''
 DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
@@ -99,7 +99,7 @@ elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
         "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
     }
 
-'''
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
