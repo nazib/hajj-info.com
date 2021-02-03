ab7654e64d5252c060643ca5d1a7c32d8fd2e66d
nazib <abdullah.nazib@gmail.com>
2021-02-03 18:09:12 +1000

    Database removed from app component

--- hajjinfo/settings.py
+++ hajjinfo/settings.py
@@ -77,6 +77,14 @@ WSGI_APPLICATION = 'hajjinfo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
+
+'''
 if DEVELOPMENT_MODE is True:
     DATABASES = {
         "default": {
@@ -91,7 +99,7 @@ elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
         "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
     }
 
-
+'''
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
