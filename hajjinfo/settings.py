ff6c58636d2c9c7c928b4007242ac3ed387e6026
Nazib <NazibA@mskcc.org>
2021-05-10 15:02:29 -0400

    checking DEBUG=False

--- hajjinfo/settings.py
+++ hajjinfo/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
-DEBUG = os.getenv("DEBUG", "True") == "True"
+DEBUG = "False"#os.getenv("DEBUG", "True") == "True"
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "True") == "True"
 # SECURITY WARNING: don't run with debug turned on in production!
